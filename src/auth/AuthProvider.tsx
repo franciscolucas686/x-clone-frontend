a887db5993f01d4400ba900b51f2e18c6270d807
Francisco Lucas <franciscolucas686@gmail.com>
2025-09-04 06:59:07 -0300

    :recycle: refactor: separate DecodedToken and User types

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -3,7 +3,7 @@ import { jwtDecode } from 'jwt-decode';
 import api from '../api/axios';
 import { AuthContext } from './AuthContext';
 
-export interface User {
+export interface DecodedToken {
   user_id: string;
   exp: number;
   iat: number;
@@ -11,23 +11,48 @@ export interface User {
   jti: string;
 }
 
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  avatar?: string;
+}
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null >(null);
 
   useEffect(() => {
-    const token = localStorage.getItem('access');
-    if (token) {
-      const decoded: User = jwtDecode(token);
-      setUser(decoded);
+    const checkUser = async () => {
+      const token = localStorage.getItem("access");
+      if (!token) return;
+      try {
+        const decoded: DecodedToken = jwtDecode(token);
+        if (decoded.exp * 1000 < Date.now()) {
+          localStorage.removeItem("access");
+          setUser(null);
+          return;
+        }
+
+        const res = await api.get(`/users/${decoded.user_id}/`);
+        setUser(res.data);
+      } catch (error) {
+        console.error("Erro ao restaurar usuário", error);
+        localStorage.removeItem("access");
+        setUser(null);
+      }
     }
+    checkUser();
   }, []);
 
   const login = async (email: string, password: string) => {
     try {
       const res = await api.post('/token/', { email, password });
       const token = res.data.access;
-    localStorage.setItem("access", token);
-      setUser(jwtDecode<User>(token));
+      localStorage.setItem("access", token);
+      
+      const decoded: DecodedToken = jwtDecode(token);
+      const userRes = await api.get(`/users/${decoded.user_id}/`);
+      setUser(userRes.data);
     } catch (error) {
       console.error("erro ao fazer login", error);
       throw error;
