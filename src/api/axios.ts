6126264d8f182510b7b8a3a11dcf213665abcba9
Francisco Lucas <franciscolucas686@gmail.com>
2025-09-24 20:54:28 -0300

    :lock: chore: add header with access token on all requests

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuth } from "../auth/useAuth";
 
 const api = axios.create({
   baseURL: "http://localhost:8000/api",
@@ -15,11 +16,10 @@ api.interceptors.request.use((config) => {
 });
 
 api.interceptors.response.use(
-  (response) => response,
+  (res) => res,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("access");
-      window.location.href = "/login";
+      useAuth().logout();
     }
     return Promise.reject(error);
   }
