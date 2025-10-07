689be03c605daa31d638a9e263f939ca8de24769
Francisco Lucas <franciscolucas686@gmail.com>
2025-10-07 05:48:02 -0300

    :lipstick: feat: create a layout modal

--- src/pages/LoginModal.tsx
+++ src/pages/LoginModal.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-
 import { useAuth } from "../auth/useAuth";
-import { CloseIcon } from "../components/icons/CloseIcon";
 import { Xlogo } from "../components/icons/Xlogo";
+import ModalLayout from "../layouts/ModalLayout";
 
 interface LoginModalProps {
   onClose: () => void;
@@ -17,55 +16,37 @@ export default function LoginModal({ onClose }: LoginModalProps) {
     try {
       await login(form.email, form.password);
       onClose();
-    } catch (error) {
-      console.error("Login falhou", error);
+    } catch (err) {
+      console.error("Login falhou", err);
     }
   };
 
   return (
-    <div
-      onClick={onClose}
-      className="fixed inset-0 bg-black/80 flex items-center justify-center z-50"
-    >
-      <div className="relative bg-white rounded w-[90%] max-w-[400px] h-[90%] max-h-[450px] px-12 flex flex-col">
-        <button
-          type="button"
-          onClick={onClose}
-          className="absolute top-2 left-2"
-        >
-          <CloseIcon />
+    <ModalLayout onClose={onClose} className="w-[400px]">
+      <Xlogo />
+      <h2 className="text-xl my-6 text-center">Entrar no X</h2>
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+        <input
+          type="text"
+          placeholder="Email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          className="p-2 border rounded focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          className="p-2 border rounded focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
+        />
+        {error && <p className="text-red-500 text-sm text-center">{error}</p>}
+        <button className="btn" type="submit" disabled={loading}>
+          {loading ? "Entrando..." : "Login"}
         </button>
-        <Xlogo />
-
-        <div className="flex flex-col justify-center">
-          <h2 className="text-xl my-[8%] text-center">Entrar no X</h2>
-
-          <form onSubmit={handleSubmit} className="flex flex-col gap-10">
-            <input
-              type="text"
-              placeholder="Email"
-              value={form.email}
-              onChange={(e) => setForm({ ...form, email: e.target.value })}
-              className="p-2 border rounded focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
-              required
-            />
-            <input
-              type="password"
-              placeholder="Senha"
-              value={form.password}
-              onChange={(e) => setForm({ ...form, password: e.target.value })}
-              className="p-2 border rounded focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
-            />
-
-            {error && (
-              <p className="text-red-500 text-sm text-center">{error}</p>
-            )}
-            <button className="btn" type="submit" disabled={loading}>
-              {loading ? "Entrando..." : "Login"}
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+      </form>
+    </ModalLayout>
   );
 }
--- src/pages/RegisterModal.tsx
+++ src/pages/RegisterModal.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../auth/useAuth";
-import { CloseIcon } from "../components/icons/CloseIcon";
 import { Xlogo } from "../components/icons/Xlogo";
+import ModalLayout from "../layouts/ModalLayout";
 
 interface RegisterModalProps {
   onClose: () => void;
@@ -24,31 +24,22 @@ export default function RegisterModal({ onClose }: RegisterModalProps) {
     setError(null);
 
     try {
-      const formData = new FormData();
-      formData.append("username", form.username);
-      formData.append("email", form.email);
-      formData.append("password", form.password);
-      formData.append("confirmPassword", form.confirmPassword);
-
       const res = await fetch("/api/register/", {
         method: "POST",
-        body: formData,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(form),
       });
 
       if (!res.ok) {
         const errorData = await res.json();
         setError(errorData.message || "Registro falhou");
-        setLoading(false);
         return;
       }
 
-      const data = await res.json();
-      console.log("Usuário criado:", data);
-
       await login(form.email, form.password);
       onClose();
-    } catch (error) {
-      console.error("Registro falhou", error);
+    } catch (err) {
+      console.error("Registro falhou", err);
       setError("Erro ao registrar usuário!");
     } finally {
       setLoading(false);
@@ -56,65 +47,50 @@ export default function RegisterModal({ onClose }: RegisterModalProps) {
   };
 
   return (
-    <div
-      onClick={onClose}
-      className="fixed inset-0 bg-black/80 flex items-center justify-center z-50"
-    >
-      <div className="relative bg-white rounded w-[90%] max-w-[400px] h-[90%] max-h-[600px] px-8 py-6 flex flex-col overflow-auto">
-        <button
-          type="button"
-          onClick={onClose}
-          className="absolute top-2 left-2"
-        >
-          <CloseIcon />
+    <ModalLayout onClose={onClose} className="w-[400px]">
+      <Xlogo />
+      <h2 className="text-2xl mb-6 text-center">Criar sua conta</h2>
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+        <input
+          type="text"
+          placeholder="Username"
+          value={form.username}
+          onChange={(e) => setForm({ ...form, username: e.target.value })}
+          className="p-2 border rounded"
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          className="p-2 border rounded"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          className="p-2 border rounded"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Confirmar senha"
+          value={form.confirmPassword}
+          onChange={(e) =>
+            setForm({ ...form, confirmPassword: e.target.value })
+          }
+          className="p-2 border rounded"
+          required
+        />
+        {error && <p className="text-red-500 text-sm text-center">{error}</p>}
+        <button type="submit" disabled={loading} className="btn">
+          {loading ? "Registrando..." : "Cadastrar"}
         </button>
-        <Xlogo />
-
-        <h2 className="text-2xl mb-6 text-center">Criar sua conta</h2>
-
-        <form onSubmit={handleSubmit} className="flex flex-col gap-10">
-          <input
-            type="text"
-            placeholder="Username"
-            value={form.username}
-            onChange={(e) => setForm({ ...form, username: e.target.value })}
-            className="p-2 border rounded"
-            required
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            value={form.email}
-            onChange={(e) => setForm({ ...form, email: e.target.value })}
-            className="p-2 border rounded"
-            required
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            value={form.password}
-            onChange={(e) => setForm({ ...form, password: e.target.value })}
-            className="p-2 border rounded"
-            required
-          />
-          <input
-            type="password"
-            placeholder="Confirmar senha"
-            value={form.confirmPassword}
-            onChange={(e) =>
-              setForm({ ...form, confirmPassword: e.target.value })
-            }
-            className="p-2 border rounded"
-            required
-          />
-
-          {error && <p className="text-red-500 text-sm text-center">{error}</p>}
-
-          <button type="submit" disabled={loading} className="btn">
-            {loading ? "Registrando..." : "Cadastrar"}
-          </button>
-        </form>
-      </div>
-    </div>
+      </form>
+    </ModalLayout>
   );
 }
