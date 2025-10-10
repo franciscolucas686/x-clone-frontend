6d5205176de2793ea9db6af79f5e62341ce738c9
Francisco Lucas <franciscolucas686@gmail.com>
2025-10-10 07:11:59 -0300

    :lipstick: feat: create logic for open and close more menu

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,20 +1,42 @@
 import { Bell, Home, Mail, MoreHorizontal, Search, User } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { Xlogo } from "../icons/Xlogo";
 import ButtonPostModal from "./ButtonPostModal";
+import MoreMenu from "./MoreMenu";
 import UserCard from "./UserCard";
+
 const navItems = [
   { icon: Home, label: "Página inicial", path: "/feed" },
   { icon: Search, label: "Explorar", path: "#" },
-  { icon: Bell, label: "Notificações", path: "#" },
-  { icon: Mail, label: "Mensagens", path: "#" },
+  { icon: Bell, label: "Notificações", path: "/notifications" },
+  { icon: Mail, label: "Mensagens", path: "/messages" },
   { icon: User, label: "Perfil", path: "/profile" },
   { icon: MoreHorizontal, label: "Mais", path: "#" },
 ];
 
 export default function Sidebar() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showMoreMenu, setShowMoreMenu] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setShowMoreMenu(false);
+      }
+    }
+
+    if (showMoreMenu) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showMoreMenu]);
 
   return (
     <div className="sticky top-0 h-screen flex flex-col justify-between p-4">
@@ -25,15 +47,30 @@ export default function Sidebar() {
           </Link>
         </div>
         <nav className="space-y-4 w-full">
-          {navItems.map(({ icon: Icon, label, path }) => (
-            <Link
-              key={label}
-              to={path}
-              className="flex p-2 px-6 cursor-pointer rounded-full hover:bg-gray-200 items-center transition-colors duration-100 ease-in-out space-x-3 font-semibold "
-            >
-              <Icon size={30} className="mr-5" /> <span>{label}</span>
-            </Link>
-          ))}
+          {navItems.map(({ icon: Icon, label, path }) =>
+            label === "Mais" ? (
+              <div key={label} ref={menuRef} className="relative" >
+                <button
+                  key={label}
+                  onClick={() => setShowMoreMenu((prev) => !prev)}
+                  className="flex p-2 px-6 cursor-pointer rounded-full hover:bg-gray-200 items-center transition-colors duration-100 ease-in-out space-x-3 font-semibold w-full text-left"
+                >
+                  <Icon size={30} className="mr-5" /> <span>{label}</span>
+                </button>
+                {showMoreMenu && <MoreMenu onClose={() => setShowMoreMenu(false)} /> }
+              </div>
+              
+
+            ) : (
+              <Link
+                key={label}
+                to={path}
+                className="flex p-2 px-6 cursor-pointer rounded-full hover:bg-gray-200 items-center transition-colors duration-100 ease-in-out space-x-3 font-semibold "
+              >
+                <Icon size={30} className="mr-5" /> <span>{label}</span>
+              </Link>
+            )
+          )}
         </nav>
         <button
           onClick={() => setIsModalOpen(true)}
