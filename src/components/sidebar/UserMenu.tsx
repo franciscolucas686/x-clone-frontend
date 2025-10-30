4d07951b21fc3d3b8812e37492c2c37ff714b027
Francisco Lucas <franciscolucas686@gmail.com>
2025-10-30 06:42:03 -0300

    :sparkles: feat: login function applied to the button

--- src/components/sidebar/UserMenu.tsx
+++ src/components/sidebar/UserMenu.tsx
@@ -1,15 +1,26 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../auth/useAuth";
 
 type UserMenuProps = {
   username: string;
 };
 
 export default function UserMenu({ username }: UserMenuProps) {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
   return (
     <div className="flex flex-col">
-      <Link to={"/"} className="px-4 py-2 text-left rounded-lg cursor-pointer hover:bg-gray-100 font-semibold transition-colors duration-200 ease-in-out">
+      <button
+        onClick={handleLogout}
+        className="px-4 py-2 text-left rounded-lg cursor-pointer hover:bg-gray-100 font-semibold transition-colors duration-200 ease-in-out"
+      >
         Sair de {username}
-      </Link>
+      </button>
     </div>
   );
 }
