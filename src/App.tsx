a32ec7130e4ea6437409b753d54d19431dae0030
Francisco Lucas <franciscolucas686@gmail.com>
2025-11-05 06:42:36 -0300

    :recycle: refactor(auth): migrate local state to redux for userauthentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { AppRoutes } from "./routes/AppRoutes";
 
-
 function App() {
   return (
     <Provider store={store}>
