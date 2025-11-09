0f3d4e20d592c16a012a0aef31e4d94a6553c5a8
Francisco Lucas <franciscolucas686@gmail.com>
2025-11-09 16:53:17 -0300

    :bug: fix: ensure follower and following counters update on profile reload

--- src/features/users/userThunks.ts
+++ src/features/users/userThunks.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../../api/axios";
 import type { User } from "./types";
+import type { RootState } from "../../app/store";
 
 export const fetchUsers = createAsyncThunk<User[]>(
   "users/fetchUsers",
@@ -12,23 +13,44 @@ export const fetchUsers = createAsyncThunk<User[]>(
 
 export const toggleFollow = createAsyncThunk<
   { userId: number; isFollowing: boolean },
-  number
->("users/toggleFollow", async (userId) => {
+  number,
+  { state: RootState }
+>("users/toggleFollow", async (userId, { getState, dispatch }) => {
   const response = await api.post(`/follow/${userId}/toggle/`);
-  const isFollowing = response.data.message === "Você começou a seguir.";
-  return { userId, isFollowing };
+  const { auth, users } = getState();
+
+  if (users.selectedUser?.username) {
+    await dispatch(fetchUserByUsername(users.selectedUser.username));
+  }
+
+  if (auth.user?.username) {
+    await dispatch(fetchUserByUsername(auth.user.username));
+  }
+
+  return { userId, isFollowing: response.data.is_following };
 });
 
 export const fetchUserByUsername = createAsyncThunk<
   User,
   string,
-  { rejectValue: string }
->("users/fetchUserByUsername", async (username, { rejectWithValue }) => {
-  try {
-    const response = await api.get(`/users/${username}/`);
-    return response.data as User;
-  } catch (error: unknown) {
-    console.error("Erro ao buscar o usuário:", error);
-    return rejectWithValue("Erro ao buscar o usuário.");
+  { state: RootState, rejectValue: string }
+>(
+  "users/fetchUserByUsername",
+  async (username, { getState,  rejectWithValue }) => {
+    try {
+      const { auth } = getState();
+      const loggerdUsername = auth.user?.username;
+
+      const endpoint =
+        username === loggerdUsername
+          ? "/profile/"
+          : `/users/${username}/`;
+
+      const response = await api.get(endpoint);
+      return response.data as User;
+    } catch (error: unknown) {
+      console.error("Erro ao buscar o usuário:", error);
+      return rejectWithValue("Erro ao buscar o usuário.");
+    }
   }
-});
+);
