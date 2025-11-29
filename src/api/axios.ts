e8cc7025fe31165eef83141946a010ce22f3fa90
Francisco Lucas <franciscolucas686@gmail.com>
2025-11-29 15:31:29 -0300

    add env variables

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://x-clone-app-ywgb.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
